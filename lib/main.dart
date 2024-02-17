e132579bb8c4f574ea504d90cc61407cb35e1c93
Abdullah Waseem <l201165@lhr.nu.edu.pk>
2024-02-17 21:08:48 +0500

    added description in the basic app

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,14 @@ import 'package:flutter/widgets.dart';
 void main() {
   runApp(const MaterialApp(
     home: HomePage(),   // HomePage is a StatelessWidget
-  ));
-}
+  ));}
+// The main function is the entry point of the Flutter application. It calls runApp to start the app with a MaterialApp. The MaterialApp is configured with a HomePage widget as its home page.
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});  
   @override
   Widget build(BuildContext context) {
+    // The Container widget is a box model that allows you to create a box with various properties such as color, padding, margin, etc.
     return Container(
       color: Colors.yellow,
       child: const Center(
@@ -19,3 +20,7 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+// Widget Hierarchy:
+// Container is the outermost widget, providing a yellow background.
+// Center is a child of the Container, ensuring that its child (Text) is centered within the container.
+// Text is the innermost widget, displaying the "Hello World" text.
